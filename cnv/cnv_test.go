affc822683a7bf2a8e8b882f0f15f0e6d5477ac0
default <urbas@etos.de>
2020-05-27 09:03:54 +0200

    weitere tests

--- cnv/cnv_test.go
+++ cnv/cnv_test.go
@@ -6,13 +6,25 @@ import (
 	"github.com/waldurbas/got/cnv"
 )
 
-func Test_checkLGX(t *testing.T) {
+func Test_checkTime(t *testing.T) {
+	var dtest = []struct {
+		s string
+		u int64
+	}{
+		{"2020-05-13 23:59:06", 1589414346},
+		{"2000-01-01 00:00:00", 946684800},
+	}
 
-	s := "20190501-20200613"
-	a := cnv.EsubStr2Int(s, 0, 8)
-	b := cnv.EsubStr2Int(s, 9, 8)
+	for _, tt := range dtest {
+		u := cnv.TimeUTC2Unix(tt.s)
 
-	if a != 20190501 || b != 20200613 {
-		t.Fatalf("s=[%s], a=[%d], b=[%d]", s, a, b)
+		if u != tt.u {
+			t.Errorf("TimeUTC2Unix(%s): soll %d, ist %d", tt.s, tt.u, u)
+		}
+
+		s := cnv.Unix2UTCTimeStr(u)
+		if s != tt.s {
+			t.Errorf("Unix2UTCTimeStr(%d): soll %s, ist %s", u, tt.s, s)
+		}
 	}
 }
