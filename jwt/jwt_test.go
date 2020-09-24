51074bbc41c9074006904d7fa3be1a6e75a3b104
default <urbas@etos.de>
2020-09-24 20:59:48 +0200

    bugfix jwt_test

--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -17,26 +17,28 @@ func Test_1(t *testing.T) {
 	fmt.Println("token:", s)
 
 	d := jwt.New()
-	if !d.Parse(s) {
+	if d.Parse(s) != nil {
 		t.Errorf("Parse fail...")
 		return
 	}
 	fmt.Println("parsed:", d)
 
-	if !d.Valid("secretKey") {
+	if d.Valid("secretKey") != nil {
 		t.Errorf("not valid.")
 		return
 	}
 
-	iat := d.AsInt64("iat")
+	iat := d.Claims.AsInt64("iat")
 	fmt.Println("iat:", time.Unix(iat, 0))
 
 	expIn := int64(20)
 	for {
 		time.Sleep(1 * time.Second)
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "expired:", d.Expired(expIn), ", at", time.Unix(iat+expIn, 0).Format("2006-01-02 15:04:05"))
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05"),
+			"expired:", time.Unix(iat+expIn, 0).Format("2006-01-02 15:04:05"),
+			", at", time.Unix(iat, 0).Format("2006-01-02 15:04:05"))
 
-		if d.Expired(expIn) {
+		if d.Expired() > expIn {
 			break
 		}
 	}
